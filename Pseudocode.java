52b122f18e4ea6e4c0ff9b729ed95aed0e7a45f6
SathiyaMoo <112923581+SathiyaMoo@users.noreply.github.com>
2022-09-08 13:00:04 +0530

    Add files via upload

---
+++ Pseudocode.java
@@ -0,0 +1,30 @@
+package week1.day2;
+
+
+
+public class Pseudocode {
+
+	public static void main(String[] args) {
+		//* Problem statement: Check if the number is positive or negative		//---------------------------------------------------------------
+		//Pseudocode:---------- 1. Initialize a number, say, int number= 35; 
+		//2. If a number is positive, print "The number is positive" 
+		//If a number is negative, print "The number is negative" 
+		//If it nether negative nor positive, 
+		//print as "The number is neither positive nor negative" 
+
+		int num=35;
+		if (num<0) 
+			System.out.println("this number is negative");
+
+		else if (num==0)
+			System.out.println("the number is neither positive nor negative");
+		else 
+			System.out.println("the given number is positive");
+			
+
+
+
+
+	}
+
+}
